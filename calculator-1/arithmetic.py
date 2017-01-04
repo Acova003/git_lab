800ab7aede64d017cb2f3a58048fd9d31bb652c7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 15:46:03 -0800

    final_calc

---
+++ calculator-1/arithmetic.py
@@ -0,0 +1,33 @@
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Return the difference of two numbers"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Return the quotient of two numbers as a float"""
+    return float(num1) / float(num2)
+
+def square(num1):
+    """Return the square of a number"""
+    return num1 * num1
+
+def cube(num1):
+    """Return the cube of a number"""
+    return num1 * num1 * num1
+
+def power(num1, exponent):
+    """Return num raised to the power of exponent"""
+    return num1 ** exponent
+
+def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
+    return num1 % num2
+
+    print "Hello"
